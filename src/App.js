c3a391d5950680f02aa1aff5046356598d100cf8
KH_trap1215 <95030019+bornracer1215@users.noreply.github.com>
2023-01-18 14:45:30 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./App.css";
+import Chat from './chat';
 import Sidebar from './sidebar';
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* sidebar */}
       <Sidebar />
       {/* chat */}
+      <Chat />
     </div>
   );
 }
