cb88477e5591812c46c5f577fd4f1bb00dd3ae6e
KH_trap1215 <95030019+bornracer1215@users.noreply.github.com>
2023-01-18 10:53:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Sidebar from './sidebar';
+function App() {
+  return (
+    <div className="app">
+      
+      {/* sidebar */}
+      <Sidebar />
+      {/* chat */}
+    </div>
+  );
+}
+
+export default App;
